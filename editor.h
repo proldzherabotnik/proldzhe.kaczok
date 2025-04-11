2600a7f53ebd0dc474e1e79c841bc803e3a33929
proldzherabotnik <nubasgirabas@icloud.com>
2025-04-11 09:20:51 +0000

    дапалнени
    типер така прекалюха шо нада патвирждене

--- editor.h
+++ editor.h
@@ -1,16 +1,29 @@
-
+#include <string.h>
 #include <stdio.h>
 int edit(){
 	// mozhno skazat hub zapisey
-	char kg[256];
-	char raz[256];
-	char ves[256];
+	char kg[256] = "hoi maya";
+	char raz[256] = "install gentoo";
+	char ves[256] = "daite denek";
+	char YepOrNop[2];
+	wybor:
 	printf("Skoljko kg\n");
 	scanf("%s", kg);
 	printf("Skoljko raz\n");
 	scanf("%s", raz);
 	printf("Kakoj TVOJ vies\n");
 	scanf("%s", ves);
+	printf("praviljno vviol? [Y/N] \n$ ");
+	scanf("%s", YepOrNop);
+	obosrals:
+	if(!strcmp(YepOrNop, "Y") || !strcmp(YepOrNop, "y")) {
+		printf("\nOke :>\n");
+	} else if(!strcmp(YepOrNop, "N") || !strcmp(YepOrNop, "n"))
+		goto wybor;
+	else {
+		printf("\nDibiluszka :rofl:\n");
+		goto obosrals;
+	}
 	FILE *barsik;
 	barsik = fopen("manager", "w");
 	fputs(kg, barsik);
